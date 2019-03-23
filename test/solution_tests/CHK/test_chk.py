dd8052e1eeadc2705e6e1aea618bcb97e5b8a5bf
sbx_user1051 <sbx_user1051@169.254.66.53>
2019-03-23 19:28:39 +0000

    Sat Mar 23 19:28:39 UTC 2019

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -4,6 +4,7 @@ from solutions.CHK.checkout_solution import checkout
 def test_checkout():
     assert checkout('AAA') == 130
     assert checkout('AAAA') == 180
+    assert checkout('AAAAA') == 200
     assert checkout('ABAB') == 145
     assert checkout('AB') == 80
     assert checkout(0) == -1
@@ -11,4 +12,5 @@ def test_checkout():
     assert checkout('C') == 20
     assert checkout('D') == 15
     assert checkout('BEE') == 80
+
 
