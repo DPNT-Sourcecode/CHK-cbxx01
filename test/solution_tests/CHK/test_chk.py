4a922294f0a85d0348e9d92ab34565f554c846b7
sbx_user1051 <sbx_user1051@169.254.66.53>
2019-03-23 18:41:01 +0000

    Sat Mar 23 18:41:01 UTC 2019

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -6,3 +6,6 @@ def test_checkout():
     assert checkout('AAAA') == 180
     assert checkout('ABAB') == 145
     assert checkout('AB') == 80
+    assert checkout(00) == -1
+    assert checkout('xyz') == -1
+
