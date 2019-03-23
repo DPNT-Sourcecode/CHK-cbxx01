d34dbfa873f79b0f53da9459cf6134f5cf660f20
sbx_user1051 <sbx_user1051@169.254.66.53>
2019-03-23 19:54:39 +0000

    Sat Mar 23 19:54:39 UTC 2019

--- test/solution_tests/CHK/test_chk.py
+++ test/solution_tests/CHK/test_chk.py
@@ -14,3 +14,12 @@ def test_checkout():
     assert checkout('BEE') == 80
     assert checkout('EE') == 80
 
+    # Result is: FAILED
+    # Some requests have failed (6/40). Here are some of them:
+    #  - {"method":"checkout","params":["AAAAAAAA"],"id":"CHK_R2_020"}, expected: 330, got: 350
+    assert checkout('AAAAAAAA') == 330
+
+    #  - {"method":"checkout","params":["AAAAAAAAA"],"id":"CHK_R2_021"}, expected: 380, got: 400
+    #  - {"method":"checkout","params":["ABCDEABCDE"],"id":"CHK_R2_038"}, expected: 280, got: 265
+
+
